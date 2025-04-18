518719ad3d7d60523f40f1d0d06d321f9cf3e90e
Diliru Nagahawatta <83059616+diliru326@users.noreply.github.com>
2025-04-18 15:32:16 +0530

    Add files via upload
    Committing API Gateway

---
+++ API-Gateway/src/test/java/com/SpringbootAcademy/API_Gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringbootAcademy.API_Gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
